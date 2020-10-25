00f119510f327430e30b9bdb1040fbfdbc6140fe
Mohammadjafari80 <mamad.jafari91@gmail.com>
2020-10-25 13:00:50 +0330

    Reformat frontend

---
+++ front/app.js
@@ -0,0 +1,7 @@
+function writeIndexedLine(lang) {
+    document.getElementById("write_answer").innerHTML = lang;
+}
+
+function sha(lang) {
+    document.getElementById("sha_answer").innerHTML = lang;
+}
