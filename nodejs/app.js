d08230cbbff66bb6c7857742e7ffe0dcdf326291
Mohammadjafari80 <mamad.jafari91@gmail.com>
2020-11-01 23:55:54 +0330

    fix nodeJS bug

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,28 +1,39 @@
-const fs = require('fs');
+const fs = require("fs");
 let fileText;
-fs.readFile('./sample.txt', 'utf8', (err,data) => fileText = data.split('\n'));
+fs.readFile(
+    "./sample.txt",
+    "utf8",
+    (err, data) => (fileText = data.split("\n"))
+);
 
-const hash = require('crypto')
-const express = require('express');
+const hash = require("crypto");
+const express = require("express");
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.post('/nodejs/sha', (req, res) => {
+app.post("/nodejs/sha", (req, res) => {
     const response = {
-        result : req.body.first + req.body.second
+        result: req.body.first + req.body.second,
     };
-    res.send({sha : hash.createHash('sha256').update(String(response.result)).digest('base64')});
-})
 
+    console.log(response.result);
 
-app.get('/nodejs/write/:id', (req, res) => {
+    res.send({
+        sha: hash
+            .createHash("sha256")
+            .update(String(response.result))
+            .digest("base64"),
+    });
+});
+
+app.get("/nodejs/write/:id", (req, res) => {
     const response = {
-        result :  String(fileText[req.params.id - 1]).replace('\r','')
+        result: String(fileText[req.params.id - 1]).replace("\r", ""),
     };
-    
+
     res.send(response.result);
-})
+});
 
 const port = 3000;
-app.listen(port, () => console.log(`listenin on port ${port}...`))
+app.listen(port, () => console.log(`listenin on port ${port}...`));
