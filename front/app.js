0bab077bb43a9829a624f2df06472e4b96b1838d
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2020-11-01 17:24:53 +0330

    Add script

--- front/app.js
+++ front/app.js
@@ -1,7 +1,21 @@
 function writeIndexedLine(lang) {
-    document.getElementById("write_answer").innerHTML = lang;
+
 }
 
 function sha(lang) {
-    document.getElementById("sha_answer").innerHTML = lang;
+    let requestBody = {};
+    requestBody["M"] = document.getElementById("M").value;
+    requestBody["N"] = document.getElementById("N").value;
+    if (requestBody["M"] == "" || requestBody["N" == ""]) {
+        document.getElementById("sha_answer").innerHTML = "Please enter two numbers.";
+        return
+    }
+    requestBody = JSON.stringify(requestBody);
+    let request = new XMLHttpRequest();
+    request.open('POST', '/' + lang + '/sha', true)
+    request.onreadystatechange = function () {
+        let res = this.response.JSON
+        document.getElementById("sha_answer").innerHTML = res["Answer"] + res["Error"]
+    }
+    request.send(requestBody);
 }
